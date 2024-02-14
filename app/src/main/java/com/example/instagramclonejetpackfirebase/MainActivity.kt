11fef3841f102b59841f38ff0114994048670cee
DatMV01 <datmv01@gmail.com>
2024-02-14 22:14:49 +0700

    Setup Hilt

---
+++ app/src/main/java/com/example/instagramclonejetpackfirebase/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.instagramclonejetpackfirebase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.instagramclonejetpackfirebase.ui.theme.InstagramCloneJetpackFirebaseTheme
+import com.example.instagramclonejetpackfirebase.viewmodel.IgViewModel
+
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InstagramCloneJetpackFirebaseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    InstagramApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun InstagramApp() {
+    val vm = hiltViewModel<IgViewModel>()
+    Text(text = "abc")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    InstagramCloneJetpackFirebaseTheme {
+        InstagramApp()
+    }
+}
---
+++ app/src/main/java/com/example/instagramclonejetpackfirebase/di/InstagramCloneApplication.kt
@@ -0,0 +1,8 @@
+package com.example.instagramclonejetpackfirebase.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class InstagramCloneApplication : Application() {
+}
